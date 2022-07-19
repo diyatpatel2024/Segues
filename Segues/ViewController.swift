4ccbe247ae43edcb19ab14b2c5b05f34d2d31344
Diya Patel <diyatpatel2024@gmail.com>
2022-07-19 10:17:39 -0500

    Initial Commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Diya Patel on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Green(_ sender: Any) {
+    }
+    
+    @IBAction func Blue(_ sender: Any) {
+    }
+    @IBAction func Red(_ sender: Any) {
+    }
+    
+    @IBAction func Paris(_ sender: Any) {
+    }
+    @IBAction func Hawaii(_ sender: Any) {
+    }
+    
+    @IBAction func Greece(_ sender: Any) {
+    }
+    @IBAction func Reading(_ sender: Any) {
+    }
+    @IBAction func Sleeping(_ sender: Any) {
+    }
+    @IBAction func Exercising(_ sender: Any) {
+    }
+    @IBAction func Again(_ sender: Any) {
+    }
+    @IBAction func Begin(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
